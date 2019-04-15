2c0add934fc253aa6ca47b2460268f3a9e5d036d
Vamankumar Patel <vpatel73@uncc.edu>
2019-04-15 01:36:27 +0000

    Generate and run Note Migration

--- noteApp/app/controllers/note_apps_controller.rb
+++ noteApp/app/controllers/note_apps_controller.rb
@@ -1,2 +1,21 @@
 class NoteAppsController < ApplicationController
+    def index 
+        
+    end
+    def new
+    end
+    
+    def create 
+        @notes = NoteApp.new(notes_params)
+        
+        @notes.save
+        redirect_to @notes
+    end
+    
+end
+
+private 
+    def notes_params
+        params.require(:notes).permit(:title, :text) 
+    end
 end
