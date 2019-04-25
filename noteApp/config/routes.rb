4cf7df109db6403e8943389f6e5e42036bd82702
Vamankumar Patel <vpatel73@uncc.edu>
2019-04-25 17:31:35 +0000

    Initialize rials for app

--- noteApp/config/routes.rb
+++ noteApp/config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  
-  resources :note_apps
-  
-  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
